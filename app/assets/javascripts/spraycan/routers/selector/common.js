b4c9d65ad74e49fab0817f2ac84e371c9d752350
Brian D. Quinn <briandquinn@gmail.com>
2012-02-27 11:44:06 +0000

    Fix re-applying active layout

--- app/assets/javascripts/spraycan/routers/selector/common.js
+++ app/assets/javascripts/spraycan/routers/selector/common.js
@@ -1,36 +1,10 @@
 Spraycan.Routers.Selector = Backbone.Router.extend({
   routes: {
- 
+
     "save": "save_active",
     "theme?apply=:cid": "apply_theme",
     "cancel_dialog": "cancel_dialog",
   },
-
-
-  // design: function(){
-  //   Spraycan.set_current('design', 'index');
-  //   var compiled = JST["spraycan/templates/navigation/design"];
-  //   $('#colors-container').html('');
-  //   $('#design-container').html(compiled());
-  //   $('#fonts-container').html('');
-  //   $('#images-container').html('');
-  // },
-
-  // design_layouts: function(){
-  //   Spraycan.set_current('design', 'layouts');
-  //   new Spraycan.Views.Shared.GroupIndex({display_name: 'Layouts', group: 'layouts'});
-  // },
-
-  // design_products_listing: function(){
-  //   Spraycan.set_current('design', 'product_listing');
-  //   new Spraycan.Views.Shared.GroupIndex({display_name: 'Product Listing', group: 'product_listing'});
-  // },
-
-  // design_products_details: function(){
-  //   Spraycan.set_current('design', 'product_details');
-  //   new Spraycan.Views.Shared.GroupIndex({display_name: 'Product Details', group: 'product_details'});
-  // },
-  //
 
   save_active: function(){
     if(Spraycan.view!=null){
@@ -38,7 +12,6 @@ Spraycan.Routers.Selector = Backbone.Router.extend({
     }
     window.location.href ="#";
   },
-
 
   apply_theme: function(cid){
     var theme = Spraycan.themes.getByCid(cid);
